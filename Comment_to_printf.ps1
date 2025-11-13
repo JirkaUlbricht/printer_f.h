b545da80561796a7bba230df3948ca52da4a97b3
Gottchabeach <jirka.ulbricht@gmail.com>
2025-11-13 15:44:43 +0100

    Powershell script for comment to printf conversion

---
+++ Comment_to_printf.ps1
@@ -0,0 +1,13 @@
+﻿
+# Change this according to your needs
+$in  = 'C:\Users\Path\To\Input\Sample.txt'
+$out = 'C:\Path\To\Output\Output.txt'
+
+
+$lines = Get-Content -LiteralPath $in -Encoding UTF8
+$escaped = $lines | ForEach-Object {
+    $_.Replace('\','\\').Replace('"','\"').Replace('%','%%') + '\n'
+}
+$text = 'printf("' + ($escaped -join '') + '");'
+
+[System.IO.File]::WriteAllText($out, $text, (New-Object System.Text.UTF8Encoding($false)))
